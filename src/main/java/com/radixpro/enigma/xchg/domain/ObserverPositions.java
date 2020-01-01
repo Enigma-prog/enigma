a3374985e9974b8e57277f2675f4aba6218e9943
Jan Kampherbeek <tech@kampherbeek.com>
2020-01-01 23:30:43 +0100

    Added enum and resourcebundle for observer positions.

---
+++ src/main/java/com/radixpro/enigma/xchg/domain/ObserverPositions.java
@@ -0,0 +1,37 @@
+/*
+ * Jan Kampherbeek, (c) 2020.
+ * Enigma is open source.
+ * Please check the file copyright.txt in the root of the source for further details.
+ */
+
+package com.radixpro.enigma.xchg.domain;
+
+public enum ObserverPositions {
+   GEOCENTRIRC(1, "geocentric"),
+   TOPOCENTRIC(2, "topocentric"),
+   HELIOCENTRIC(3, "heliocentric");
+
+   private static final String RB_PREFIX = "gen.lookup.observerpositions.";
+   private static final String RB_NAME_POSTFIX = ".name";
+   private static final String RB_DESCRIPTION_POSTFIX = ".description";
+   private final int id;
+   private final String nameForRB;
+
+   ObserverPositions(final int id, final String nameForRB) {
+      this.id = id;
+      this.nameForRB = nameForRB;
+   }
+
+   public int getId() {
+      return id;
+   }
+
+   public String getRbKeyForName() {
+      return RB_PREFIX + nameForRB + RB_NAME_POSTFIX;
+   }
+
+   public String getRbKeyForDescription() {
+      return RB_PREFIX + nameForRB + RB_DESCRIPTION_POSTFIX;
+   }
+
+}
---
+++ src/test/java/com/radixpro/enigma/xchg/domain/ObserverPositionsTest.java
@@ -0,0 +1,31 @@
+/*
+ * Jan Kampherbeek, (c) 2020.
+ * Enigma is open source.
+ * Please check the file copyright.txt in the root of the source for further details.
+ */
+
+package com.radixpro.enigma.xchg.domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ObserverPositionsTest {
+
+   private ObserverPositions observerPosition = ObserverPositions.HELIOCENTRIC;
+
+   @Test
+   public void getId() {
+      assertEquals(3, observerPosition.getId());
+   }
+
+   @Test
+   public void getRbKeyForName() {
+      assertEquals("gen.lookup.observerpositions.heliocentric.name", observerPosition.getRbKeyForName());
+   }
+
+   @Test
+   public void getRbKeyForDescription() {
+      assertEquals("gen.lookup.observerpositions.heliocentric.description", observerPosition.getRbKeyForDescription());
+   }
+}
