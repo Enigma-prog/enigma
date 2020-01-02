6af5fcb137d4f7a0793a75ddd1029a372aec730e
Jan Kampherbeek <tech@kampherbeek.com>
2020-01-02 10:56:59 +0100

    Added enum and resourcebundle for Ratings.

---
+++ src/main/java/com/radixpro/enigma/xchg/domain/Ratings.java
@@ -0,0 +1,41 @@
+/*
+ * Jan Kampherbeek, (c) 2020.
+ * Enigma is open source.
+ * Please check the file copyright.txt in the root of the source for further details.
+ */
+
+package com.radixpro.enigma.xchg.domain;
+
+public enum Ratings {
+   AA(1, "aa"),
+   A(2, "a"),
+   B(3, "b"),
+   C(4, "c"),
+   DD(5, "dd"),
+   X(6, "x"),
+   XX(7, "XX");
+
+   private static final String RB_PREFIX = "gen.lookup.ratings.";
+   private static final String RB_NAME_POSTFIX = ".name";
+   private static final String RB_DESCRIPTION_POSTFIX = ".description";
+   private final int id;
+   private final String nameForRB;
+
+   Ratings(final int id, final String nameForRB) {
+      this.id = id;
+      this.nameForRB = nameForRB;
+   }
+
+   public int getId() {
+      return id;
+   }
+
+   public String getRbKeyForName() {
+      return RB_PREFIX + nameForRB + RB_NAME_POSTFIX;
+   }
+
+   public String getRbKeyForDescription() {
+      return RB_PREFIX + nameForRB + RB_DESCRIPTION_POSTFIX;
+   }
+
+}
---
+++ src/test/java/com/radixpro/enigma/xchg/domain/RatingsTest.java
@@ -0,0 +1,36 @@
+/*
+ * Jan Kampherbeek, (c) 2020.
+ * Enigma is open source.
+ * Please check the file copyright.txt in the root of the source for further details.
+ */
+
+package com.radixpro.enigma.xchg.domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RatingsTest {
+
+   private final Ratings rating = Ratings.C;
+
+   @Test
+   public void getId() {
+      assertEquals(4, rating.getId());
+   }
+
+   @Test
+   public void getRbKeyForName() {
+      assertEquals("gen.lookup.ratings.c.name", rating.getRbKeyForName());
+   }
+
+   @Test
+   public void getRbKeyForDescription() {
+      assertEquals("gen.lookup.ratings.c.description", rating.getRbKeyForDescription());
+   }
+
+   @Test
+   public void total() {
+      assertEquals(7, Ratings.values().length);
+   }
+}
