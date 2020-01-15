c6d4fa44feba78be8457b83a8d3072c55db1890a
Jan Kampherbeek <tech@kampherbeek.com>
2020-01-15 22:42:03 +0100

    Defined actions for button and menu item to calculate new chart.

--- src/main/java/com/radixpro/enigma/ui/charts/ChartsStart.java
+++ src/main/java/com/radixpro/enigma/ui/charts/ChartsStart.java
@@ -6,12 +6,34 @@
 
 package com.radixpro.enigma.ui.charts;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ChartsStart {
 
    @FXML
    String lblTitle;
+
+   @FXML
+   void onNewChart(ActionEvent event) throws IOException {
+      FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/chartsinput.fxml"));
+      Parent parent = fxmlLoader.load();
+      ChartsInput chartsInput = fxmlLoader.getController();
+      // setObservableList om resultaten terug te krijgen
+
+      Scene scene = new Scene(parent, 300, 200);
+      Stage stage = new Stage();
+      stage.initModality(Modality.APPLICATION_MODAL);
+      stage.setScene(scene);
+      stage.showAndWait();
+   }
 
 
    public void initialize() {
