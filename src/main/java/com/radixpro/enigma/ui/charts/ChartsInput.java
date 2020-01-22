34587d102ede605e55ac523eecf7db6f55b69f27
Jan Kampherbeek <tech@kampherbeek.com>
2020-01-22 21:55:10 +0100

    Layout ChartsInput ok.

--- src/main/java/com/radixpro/enigma/ui/charts/ChartsInput.java
+++ src/main/java/com/radixpro/enigma/ui/charts/ChartsInput.java
@@ -6,21 +6,48 @@
 
 package com.radixpro.enigma.ui.charts;
 
+import com.radixpro.enigma.xchg.domain.ChartTypes;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import org.controlsfx.control.CheckComboBox;
 
 import java.io.IOException;
 
 public class ChartsInput {
 
    @FXML
-   public TextField tfieldName;
+   public TextField name;
+   @FXML
+   public TextField source;
+   @FXML
+   public TextField description;
+   @FXML
+   public TextField locationName;
+   @FXML
+   public TextField longitudeValue;
+   @FXML
+   public TextField latitudeValue;
+   @FXML
+   public TextField date;
+   @FXML
+   public TextField time;
+   @FXML
+   public CheckComboBox subject;
+   @FXML
+   public CheckComboBox rating;
 
 
    @FXML
    void onCalculate(ActionEvent event) throws IOException {
-      System.out.println("Name is " + tfieldName.getText());
+      System.out.println("Name is " + name.getText());
+   }
+
+   public void initialize() {
+
+      subject.getItems().add(ChartTypes.UNKNOWN.name());
+      subject.getItems().add(ChartTypes.NATAL.name());
+
    }
 
 }
